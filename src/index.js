e0e70024b690037acc38e4c89b6c833825c2f082
Olga Sokolianska <99847102+Sokolianska@users.noreply.github.com>
2022-06-15 18:07:46 +0200

    forecast for a week with dynamic HTML in JS

--- src/index.js
+++ src/index.js
@@ -64,6 +64,13 @@ changeDay();
 window.addEventListener("load", changeTime);
 window.addEventListener("load", changeDay);
 
+function getForecast(coordinates) {
+    
+    let apiKey = "e0dced0781a18aa3906255142be14578";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+}
+
 ///searching
 function displayWeatherCondition(response) {
   document.querySelector("#city").innerHTML = response.data.name;
@@ -82,7 +89,9 @@ function displayWeatherCondition(response) {
    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   iconElement.setAttribute("alt", response.data.weather[0].description);
   
-   celsiusTemperature = response.data.main.temp;
+  celsiusTemperature = response.data.main.temp;
+  
+  getForecast(response.data.coord);  
 }
 
 function searchCity(city) {
@@ -109,15 +118,6 @@ function getCurrentLocation(event) {
   navigator.geolocation.getCurrentPosition(searchLocation);
 }
 
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", handleSubmit);
-
-let currentLocationButton = document.querySelector("#current-location-button");
-currentLocationButton.addEventListener("click", getCurrentLocation);
-
-searchCity("Dnipro");
-
-
 //fahrenheit - link
 function displayFahrenheitTemperature(event) {
     event.preventDefault();
@@ -143,3 +143,54 @@ let celsiusTemperature = null;
 
 let celsiusLink = document.querySelector("#temp-celsius");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+//forecast
+function formatDay(timestamp){
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[day];
+}
+
+
+function displayForecast(response) {
+    
+    let forecast = response.data.daily;
+
+    let forecastElement = document.querySelector("#forecast");
+
+    let forecastHTML = `<div class="row">`;
+    forecast.forEach(function (forecastDay, index) {
+        if (index < 6) {
+            forecastHTML =
+                forecastHTML + `<div class="col-2">
+            <div class="weather-forecast-date">
+    ${formatDay(forecastDay.dt)}</div>
+        <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+        alt = ""
+        width = "42"/>
+    <div class="weather-forecast-temperatures">
+    <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}°</span>
+    <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}°</span>
+    </div></div>`;
+        }
+    });
+
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "e0dced0781a18aa3906255142be14578";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+searchCity("Dnipro");
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
